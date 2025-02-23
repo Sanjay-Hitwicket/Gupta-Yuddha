ae01b87fff0860065dbd2d2c20d742a332c975d8
Sanjay-Hitwicket <sanjay@hitwicket.com>
2025-02-24 00:42:36 +0530

    added support for scene redirection

--- Assets/Scripts/View/PlayButtonView.cs
+++ Assets/Scripts/View/PlayButtonView.cs
@@ -1,21 +1,29 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayButtonView : MonoBehaviour
 {
-    [SerializeField] private Transform playButton;
+    [SerializeField] private Transform playButtonTransform;
     private float scaleFactor = 1.2f;
     private float animationDuration = 1f;
+    [SerializeField] private Button playButton;
 
     private void OnEnable()
     {
         PlayButtonAnimation();
+        playButton.onClick.RemoveAllListeners();
+        playButton.onClick.AddListener(() => {
+            SceneManager.LoadScene("GameScene");
+            }
+        );
     }
 
     private void PlayButtonAnimation()
     {
-        playButton.DOScale(scaleFactor, animationDuration)
+        playButtonTransform.DOScale(scaleFactor, animationDuration)
             .SetEase(Ease.InOutSine)
             .SetLoops(-1, LoopType.Yoyo);
     }
