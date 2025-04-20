1b5285d25041f3b1e2e55166f8deb2b38f4d3aed
Sanjay-Hitwicket <sanjay@hitwicket.com>
2025-04-21 01:17:54 +0530

    added joystick

---
+++ Assets/Scripts/View/GestureView.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace InputControls
+{
+    public class GestureView : MonoBehaviour
+    {
+        [SerializeField] private InputAction touchAction;
+
+        private void OnEnable()
+        {
+            touchAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            touchAction.Disable();
+        }
+
+        private void Update()
+        {
+            if (touchAction != null) {
+                if (touchAction.WasPressedThisFrame()) // Equivalent to Input.GetKeyDown()
+                {
+                    Debug.Log("Touch Started");
+                }
+            }
+            else
+            {
+                Debug.LogError("Touch Action is not assigned! Assign it in the Inspector.");
+            }
+        }
+
+    }
+}
